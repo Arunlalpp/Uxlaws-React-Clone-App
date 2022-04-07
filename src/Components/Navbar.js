ef00a933804c7d37be9438d6a40f6328d8ff3832
arunlalpp <arunlalpp@gmail.com>
2022-04-07 17:54:06 +0530

    Nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div>
+            <nav className='main'>
+                <ul>
+                    <li><a href='/'>LAWS OF UX</a></li>
+                </ul>
+                <div className='nav-toggle'>
+                    <button className='nav-button'>
+                        <span className='button-text'>Menu</span> 
+                        <div className='toggle'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 24">
+                            <path
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" />
+                        </svg>
+                        </div>
+                    </button>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./Components/**/ */.{html.js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
