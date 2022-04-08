7f5e18314350188de88bb3c4c93c842eb604193d
arunlalpp <arunlalpp@gmail.com>
2022-04-08 10:20:02 +0530

    button

---
+++ src/Components/CardBoxLeft.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CardBoxLeft() {
+  return (
+    <div className='flex-button'>
+        <button className='end-button'>LEARN MORE</button>
+    </div>
+  )
+}
+
+export default CardBoxLeft
