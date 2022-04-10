4d30f280144e9f1f73b83863dda60c7c95079a3e
arunlalpp <arunlalpp@gmail.com>
2022-04-10 23:01:10 +0530

    Style

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
     <div className="App">
       {/* <Navbar /> */}
       <CardBoxes />
-      {/* <CardBoxLef para = "Productivity soars when a computer and its users interact at a pace 400ms that ensures that neither has to wait on the other. "
+      <CardBoxLef para = "Productivity soars when a computer and its users interact at a pace 400ms that ensures that neither has to wait on the other. "
       head = "Doherty Threshold" />
-      <CardBoxes head = "Hick's Law"/>
-      <CardBoxLef /> */}
-      <CardBoxes />
+      {/* <CardBoxes head = "Hick's Law"/> */}
+      {/* <CardBoxLef /> */}
+      {/* <CardBoxes /> */}
     </div>
   );
 }
