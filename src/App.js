0c01092c789f9743e95f241da61bbdbb89a8d2b1
arunlalpp <arunlalpp@gmail.com>
2022-04-13 16:19:49 +0530

    route

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import './Components/CardboxLeftStyle.css';
 import './Components/ContainerStyle.css';
 import './Components/ButtonStyle.css';
 import CardBoxes from "./Components/CardBoxes";
-import About from "./About";
+import About from "./Components/About";
 import Welcome from "./Components/Welcome";
-import {BrowserRouter, Route} from 'react-router-dom'; 
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'; 
 
 
 function App() {
   return (
-   
-   <BrowserRouter>
-   <Route path="/" Components={CardBoxes} />   
-   <Route path="/About" src={About}/>   
-   </BrowserRouter>
+   <div className="App">
+   <Router>
+   <Route path='/' component={CardBoxes} exact/>   
+   <Route path='/About' component={About} exact/>   
+   </Router>
+   </div>
    
   );
 }
