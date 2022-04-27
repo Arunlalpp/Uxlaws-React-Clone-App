2e8ee304cffc818b95120a014ccdb0cdbfaa057f
arunlalpp <arunlalpp@gmail.com>
2022-04-27 10:27:39 +0530

    Prev-next

--- src/Components/CardButton.js
+++ src/Components/CardButton.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function CardButton() {
   return (
     <div className='flex-button'>
-      <Link to = "/Page1">
-        <button  className='end-button'>Learn more</button> </Link>
+      <>
+      <button  to='/Page1'  className='end-button'>Learn more</button>
+        </>
     </div>
   )
 }
